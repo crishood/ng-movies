8070b9c8bf4aeda6133cf236caf27543cd5c3179
cristhiancepeda <cristhian.cepeda@wearemo.com>
2025-04-14 14:22:28 -0500

    feat(movie-item): add content projection

--- src/app/movie-item/movie-item.component.ts
+++ src/app/movie-item/movie-item.component.ts
@@ -1,8 +1,8 @@
-import {Component, input, output} from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { Movie } from '../model/movie.model';
-import {MillionDollarPipe} from '../pipes/million-dollar.pipe';
-import {MinToDurationPipe} from '../pipes/min-to-duration.pipe';
-import {RouterLink} from '@angular/router';
+import { MillionDollarPipe } from '../pipes/million-dollar.pipe';
+import { MinToDurationPipe } from '../pipes/min-to-duration.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-item',
@@ -10,29 +10,27 @@ import {RouterLink} from '@angular/router';
     <div class="movie-item">
       <div>
         <h4>
-          <span class="icon-star" [class.active]="isFavorite()" (click)="toggleFavorite.emit(movie())"></span>
+          <span
+            class="icon-star"
+            [class.active]="isFavorite()"
+            (click)="toggleFavorite.emit(movie())"
+          ></span>
           {{ movie().title }}
         </h4>
         <small class="subtitle">
           <span>Release date: {{ movie().release_date }}</span>
-          <span>Budget:  {{ movie().budget | millionDollar }} </span>
+          <span>Budget: {{ movie().budget | millionDollar }} </span>
           <span>Duration: {{ movie().duration | minToDuration }}</span>
         </small>
       </div>
-
-      <button [routerLink]="'details/'+ movie().id">Details</button>
+      <ng-content></ng-content>
     </div>
   `,
-  imports: [
-    MillionDollarPipe,
-    MinToDurationPipe,
-    RouterLink
-  ],
-  styleUrls: ['movie-item.component.scss']
+  imports: [MillionDollarPipe, MinToDurationPipe],
+  styleUrls: ['movie-item.component.scss'],
 })
 export class MovieItemComponent {
-  movie  = input.required<Movie>();
+  movie = input.required<Movie>();
   isFavorite = input<boolean>(false);
   toggleFavorite = output<Movie>();
 }
-
